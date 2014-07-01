f181dd33740655a32cd01fddd9b159c299a4ef7f
Sebastien Blanc <scm.blanc@gmail.com>
2014-07-01 15:29:06 +0200

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,20 @@
+angular.module('starter.controllers', [])
+ 
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+ 
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+
+  $scope.friend = Friends.get($stateParams.friendId);
+  $scope.dolphin = "dolphin";
+  
+})
+
+
+.controller('AccountCtrl', function($scope ) {
+});
